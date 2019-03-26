6129e49afc469c97581f46f0abd7a515f69be669
chaikai <cckk1995@icloud.com>
2019-03-26 10:14:02 +0800

    分类对接

---
+++ src/axios_config.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// axios.defaults.baseURL = 'http://139.198.15.172:8080';
+axios.defaults.baseURL = 'http://localhost:8090';
+axios.defaults.headers = {
+
+  // 'Content-Type': 'application/json;charset=utf-8',
+  'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8',
+  'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+  'Access-Control-Allow-Origin': '*'
+  // 'Access-Control-Allow-Headers':'Content-Type,Accept,Content-Length, Authorization, Accept,X-Requested-With'
+};
+
+axios.interceptors.response.use(function (config) {
+  // 在发送请求之前做什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+export default axios;
